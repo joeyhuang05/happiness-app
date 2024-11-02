e1f2c810549c4844e94518d2c3b6ce3ca6e05d59
wongcolin45 <wongcolin45@gmail.com>
2024-11-02 11:52:45 -0400

    updated routes

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, request, jsonify
+from weighted_average import get_weighted_average
 
 app = Flask(__name__)
 
+scores = []
 
-@app.route('/', methods=['GET'])
-def check_server():
-    #data = request.json  
-    return jsonify({'server-response' : 'okay'})
+@app.rouse('/reset_scores', methods=['GET'])
+def reset_scores():
+    scores = []
+    return jsonify({'reset scores to '}), 200
 
 
-@app.route('/happiness', methods=['POST'])
+@app.route('/get_happinese', methods=['GET'])
+def get_happinese():
+    score = get_weighted_average(scores, 0.3)
+    return jsonify({'score' : score}), 200
+
+
+@app.route('/happiness', methods=['PUT'])
 def calculate_happiness():
-    #data = request.json  
-    return jsonify({'Happinese score ': 'good'})
+    # { score : number }
+    data = request.json  
+    scores.append(data.score)
+    return '', 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
